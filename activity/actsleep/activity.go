ff8c8262da7fe8897b536074902253dd23713444
ecarlier <ecarlier@tibco.com>
2018-07-09 17:51:03 +0200

    initial version

---
+++ activity/actsleep/activity.go
@@ -0,0 +1,36 @@
+package actsleep
+
+import (
+	"time"
+
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+var log = logger.GetLogger("activity-ecarlier-passthru")
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
+
+	// do eval
+	timeMs := context.GetInput("time_ms").(int)
+
+	time.Sleep(time.Duration(timeMs) * time.Millisecond)
+
+	return true, nil
+}
